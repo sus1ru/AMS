31abd804860b28b5ec522c1744ed208bd6fd1728
sus1ru <sus1ru.s4n@gmail.com>
2026-06-07 13:24:59 +0545

    feat: added dynamic url routing in the request handler and renamed database init function

--- backend/server.py
+++ backend/server.py
@@ -1,18 +1,84 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
+import json
 
-from backend.database import create_tables
+from backend.database import setup_db
+from backend.urls import url_router
 from backend.config import settings
 
+
+class InvalidPayload(Exception):
+    pass
+
+class MethodNotAllowed(Exception):
+    pass
+
+class RouteDoesnotExist(Exception):
+    pass
+
+
 class RequestHandler(BaseHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header("Content-Type", "text/plain")
+    def send_json(self, data, status_code=200) -> None:
+        self.send_response(status_code)
+        self.send_header("Content-Type", "application/json")
         self.end_headers()
-        self.wfile.write(b"Hello from python HTTP server")
+        self.wfile.write(json.dumps(data).encode())
+    
+    def pre_process_request(self, path, incoming_method):
+        if path not in url_router.URL_MAPPINGS:
+            raise RouteDoesnotExist()
+
+        view, allowed_method = url_router.map_url(self.path)
+
+        if incoming_method != allowed_method:
+            raise MethodNotAllowed()
+
+        return view
+
+    def process_payload(self) -> dict:
+        content_length = int(self.headers.get("Content-Length", 0))
+        body = self.rfile.read(content_length)
+
+        try:
+            payload = json.loads(body)
+        except json.JSONDecodeError as e:
+            raise InvalidPayload()
+
+        return payload
+
+    def do_GET(self) -> None:
+        try:
+            view = self.pre_process_request(self.path, 'GET')
+        except MethodNotAllowed as e:
+            self.send_json({"error": "Method not allowed"}, 405)
+            return
+        except RouteDoesnotExist as e:
+            self.send_json({"error": "Route not found"}, 404)
+            return
+
+        response, status_code = view()
+        self.send_json(response, status_code)
+
+    def do_POST(self) -> None:
+        try:
+            view = self.pre_process_request(self.path, 'POST')
+        except MethodNotAllowed as e:
+            self.send_json({"error": "Method not allowed"}, 405)
+            return
+        except RouteDoesnotExist as e:
+            self.send_json({"error": "Route not found"}, 404)
+            return
+
+        try:
+            data = self.process_payload()
+        except InvalidPayload as e:
+            self.send_json({"error": "Invalid Payload"}, 400)
+
+        response, status_code = view(data)
+        self.send_json(response, status_code)
 
 
 def runserver(server_class=HTTPServer, handler_class=RequestHandler):
-    create_tables()
+    setup_db()
     server_address = (settings.server_host, settings.server_port)
     httpd = server_class(server_address, handler_class)
     print(f"Server running on http://{settings.server_host}:{settings.server_port}")
