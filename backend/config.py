66e25858e441eee1a2ce4fcd2fcfe959501fa551
sus1ru <sus1ru.s4n@gmail.com>
2026-06-07 13:25:32 +0545

    fix: changed default values for some settings variables

--- backend/config.py
+++ backend/config.py
@@ -24,9 +24,9 @@ class Settings:
     app_name: str = os.getenv('APP_NAME', default='AMS')
 
     server_host: str = os.getenv('SERVER_HOST', default='127.0.0.1')
-    server_port: int = int(os.getenv('SERVER_PORT', default="8000"))
+    server_port: int = int(os.getenv('SERVER_PORT', default='8000'))
 
-    db_name: str = os.getenv('DB_NAME', default='ams.db')
+    db_name: str = os.getenv('DB_NAME', default='backend/ams.db')
 
 
 settings = Settings()
