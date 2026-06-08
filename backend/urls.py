b443a9c71ae712a2f102a72a2c89888bbfeec7df
sus1ru <sus1ru.s4n@gmail.com>
2026-06-08 07:06:40 +0545

    feat: added the api endpoint for user registration and users list

--- backend/urls.py
+++ backend/urls.py
@@ -1,6 +1,6 @@
+from backend.auth.views import user_list_view, user_register_view
 from backend.config import settings
 from backend.core.router import url_router
-
 
 def root_view():
     return {
@@ -11,3 +11,5 @@ def root_view():
     }, 200
 
 url_router.add('/', 'GET', root_view)
+url_router.add('/users', 'GET', user_list_view)
+url_router.add('/register', 'POST', user_register_view)
