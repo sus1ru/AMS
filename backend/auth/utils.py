efc72a37bbc3b4e19b9f248837ae3ae5e42a3320
sus1ru <sus1ru.s4n@gmail.com>
2026-06-09 10:42:36 +0545

    feat: added util for verifying hashed password

--- backend/auth/utils.py
+++ backend/auth/utils.py
@@ -11,3 +11,17 @@ def hash_password(password):
     )
 
     return f"{salt.hex()}:{password_hash.hex()}"
+
+
+def verify_password(password, stored_password):
+    salt_hex, hash_hex = stored_password.split(":")
+
+    salt = bytes.fromhex(salt_hex)
+    password_hash = hashlib.pbkdf2_hmac(
+        "sha256",
+        password.encode(),
+        salt,
+        100000,
+    )
+
+    return password_hash.hex() == hash_hex
