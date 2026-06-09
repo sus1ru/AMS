c3281209a31fad470e5b4f52f5f59b3c861d32c1
sus1ru <sus1ru.s4n@gmail.com>
2026-06-09 10:44:14 +0545

    feat: added decorator function to make endpoint registeration easy

--- backend/core/router.py
+++ backend/core/router.py
@@ -2,11 +2,23 @@ class UrlRouter:
     URL_MAPPINGS = {}
 
     @classmethod
-    def add(cls, path, method, view):
-        cls.URL_MAPPINGS[path] = (view, method)
+    def add(cls, path, method, authenticated, view):
+        cls.URL_MAPPINGS[path] = (view, method, authenticated)
 
     @classmethod
     def map_url(cls, path):
         return cls.URL_MAPPINGS.get(path)
 
 url_router = UrlRouter()
+
+
+def route(path, method="GET", authenticated=False):
+    def decorator(func):
+        url_router.add(
+            path,
+            method,
+            authenticated,
+            func
+        )
+        return func
+    return decorator
