21e5949db0380d56175873de3497012c9e8f38f3
sus1ru <sus1ru.s4n@gmail.com>
2026-06-09 10:41:23 +0545

    feat: addded session time to live in .env

--- backend/config.py
+++ backend/config.py
@@ -27,6 +27,7 @@ class Settings:
     server_port: int = int(os.getenv('SERVER_PORT', default='8000'))
 
     db_name: str = os.getenv('DB_NAME', default='backend/ams.db')
+    session_ttl_hours: int = int(os.getenv('SESSION_TTL_HOURS', default='24'))
 
 
 settings = Settings()
