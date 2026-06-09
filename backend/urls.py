0431a9746bbfb2fe995fe51a0697b70c584adc6a
sus1ru <sus1ru.s4n@gmail.com>
2026-06-09 10:47:51 +0545

    ref: cleanup in the endpoint registeration in urls

--- backend/urls.py
+++ backend/urls.py
@@ -1,4 +1,3 @@
-from backend.auth.views import user_list_view, user_register_view
 from backend.config import settings
 from backend.core.router import url_router
 
@@ -10,6 +9,4 @@ def root_view():
         ]
     }, 200
 
-url_router.add('/', 'GET', root_view)
-url_router.add('/users', 'GET', user_list_view)
-url_router.add('/register', 'POST', user_register_view)
+url_router.add('/', 'GET', False, root_view)
