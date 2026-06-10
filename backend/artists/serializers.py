e69f17f3e20d067fe3cc6b5bfeb5b1be64141418
sus1ru <sus1ru.s4n@gmail.com>
2026-06-10 19:38:34 +0545

    feat: added serializers and field level validation for artists

---
+++ backend/artists/serializers.py
@@ -0,0 +1,45 @@
+from datetime import datetime, UTC
+
+from backend.auth.serializers import dob_not_in_future
+from backend.core.exceptions import FieldValidationError
+from backend.core.serializers import CharField, DateTimeField, IntegerField, Serializer
+from backend.database import ALLOWED_GENDERS
+
+def first_release_year_not_in_future(value):
+    today = datetime.now(UTC).year
+    if value > today:
+        raise FieldValidationError("Field cannot be in the future")
+
+class ArtistCreateSerializer(Serializer):
+    fields = {
+        "name": CharField(max_length=255),
+        "dob": DateTimeField(
+            required=False,
+            blank=True,
+            null=True,
+            custom_validation=dob_not_in_future,
+        ),
+        "gender": CharField(
+            max_length=4,
+            required=False,
+            blank=True,
+            null=True,
+            choices=ALLOWED_GENDERS,
+        ),
+        "address": CharField(max_length=255, required=False, blank=True, null=True),
+        "first_release_year": IntegerField(
+            required=False,
+            null=True,
+            custom_validation=first_release_year_not_in_future,
+        ),
+        "no_of_albums_released": IntegerField(required=False, null=True),
+    }
+
+
+class ArtistUpdateSerializer(ArtistCreateSerializer):
+    fields = {
+        key: field for key, field in ArtistCreateSerializer.fields.items()
+    }
+
+    for field in fields.values():
+        field.required = False
