c633aacd97e0f52f4fa940bf7250713b7e0a54d3
sus1ru <sus1ru.s4n@gmail.com>
2026-06-10 08:09:14 +0545

    ref: separated enum choices in a variable

--- backend/database.py
+++ backend/database.py
@@ -2,6 +2,9 @@ import sqlite3
 
 from backend.config import settings
 
+ALLOWED_ROLES = ["super_admin", "artist_manager", "artist"]
+ALLOWED_GENDERS = ["m", "f", "o"]
+ALLOWED_GENRES = ['rnb', 'country', 'classic', 'rock', 'jazz']
 
 def get_connection():
     conn = sqlite3.connect(settings.db_name)
@@ -22,29 +25,29 @@ def setup_db():
             password VARCHAR(500) NOT NULL,
             phone VARCHAR(20),
             dob DATETIME,
-            gender TEXT CHECK (gender IN ('m', 'f', 'o')),
+            gender TEXT CHECK (gender IN ({genders})),
             address VARCHAR(255),
             role TEXT NOT NULL CHECK (
-                role IN ('super_admin', 'artist_manager', 'artist')
+                role IN ({roles})
             ),
-            created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-            updated_at DATETIME DEFAULT CURRENT_TIMESTAMP
+            created_at TEXT DEFAULT (strftime('%Y-%m-%dT%H:%M:%f+00:00', 'now')),
+            updated_at TEXT DEFAULT (strftime('%Y-%m-%dT%H:%M:%f+00:00', 'now'))
         )
-    """)
+    """.format(roles=str(ALLOWED_ROLES)[1:-1], genders=str(ALLOWED_GENDERS)[1:-1]))
 
     cursor.execute("""
         CREATE TABLE IF NOT EXISTS artists (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             name VARCHAR(255) NOT NULL,
             dob DATETIME,
-            gender TEXT CHECK (gender IN ('m', 'f', 'o')),
+            gender TEXT CHECK (gender IN ({genders})),
             address VARCHAR(255),
             first_release_year INTEGER,
             no_of_albums_released INTEGER DEFAULT 0,
-            created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-            updated_at DATETIME DEFAULT CURRENT_TIMESTAMP
+            created_at TEXT DEFAULT (strftime('%Y-%m-%dT%H:%M:%f+00:00', 'now')),
+            updated_at TEXT DEFAULT (strftime('%Y-%m-%dT%H:%M:%f+00:00', 'now'))
         )
-    """)
+    """.format(genders=str(ALLOWED_GENDERS)[1:-1]))
 
     cursor.execute("""
         CREATE TABLE IF NOT EXISTS songs (
@@ -53,22 +56,22 @@ def setup_db():
             title VARCHAR(255) NOT NULL,
             album_name VARCHAR(255),
             genre TEXT CHECK (
-                genre IN ('rnb', 'country', 'classic', 'rock', 'jazz')
+                genre IN ({genres})
             ),
-            created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-            updated_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+            created_at TEXT DEFAULT (strftime('%Y-%m-%dT%H:%M:%f+00:00', 'now')),
+            updated_at TEXT DEFAULT (strftime('%Y-%m-%dT%H:%M:%f+00:00', 'now')),
 
             FOREIGN KEY (artist_id) REFERENCES artists(id)
                 ON DELETE CASCADE
         )
-    """)
+    """.format(genres=str(ALLOWED_GENRES)[1:-1]))
 
     cursor.execute("""
         CREATE TABLE IF NOT EXISTS sessions (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             session_id VARCHAR(255) NOT NULL UNIQUE,
             user_id INTEGER NOT NULL,
-            created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+            created_at TEXT DEFAULT (strftime('%Y-%m-%dT%H:%M:%f+00:00', 'now')),
             expires_at DATETIME,
             FOREIGN KEY (user_id) REFERENCES users(id)
                 ON DELETE CASCADE
